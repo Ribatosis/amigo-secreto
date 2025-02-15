4de60622920001e54f9f8c26e4cefc78b36e1a67
Ribatosis <juliana.espinosa@genstudents.org>
2025-02-15 16:31:04 -0500

    Función sorteo con números aleatorios

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 let listaAmigos = [ ];
-let titulo = document.querySelector('h2');
+let titulo = document.getElementById('tituloH2');
 
 function agregarAmigo() {
     let amigos = document.getElementById('amigo').value; 
+
+    titulo.innerHTML = "Digite el nombre de sus amigos"
 
     if (amigos !== ""){
         listaAmigos.push(" " + amigos);
         mostrarLista();
         limpiarCaja();
     }else{
-        alert("Ingresa un nombre válido")
+        titulo.innerHTML = "Ingresa un nombre válido";
     }
 
 }
@@ -18,6 +20,24 @@ function mostrarLista(){
     document.getElementById('listaAmigos2').innerHTML = listaAmigos;
 }
 
+function sortearAmigo() {
+
+    let numeroMaximo = listaAmigos.length - 1
+
+    if (listaAmigos.length > 1){
+        let aleatorio = numeroAleatorio(0, numeroMaximo)
+        titulo.innerHTML = "Tu amigo secreto es: " + listaAmigos[aleatorio];
+    }else{
+        titulo.innerHTML = "Ingrese más nombres por favor"
+    }
+}
+
+
 function limpiarCaja(){
     let cajaVacia = document.getElementById('amigo').value = '';
+}
+
+
+function numeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
