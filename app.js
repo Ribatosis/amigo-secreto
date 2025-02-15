517be78e7ea212a916f5c1d4dc339c59be88dd5e
Ribatosis <juliana.espinosa@genstudents.org>
2025-02-15 15:46:40 -0500

    Funciones Agregar amigos y mostrar lista

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [ ];
+let titulo = document.querySelector('h2');
+
+function agregarAmigo() {
+    let amigos = document.getElementById('amigo').value; 
+
+    if (amigos !== ""){
+        listaAmigos.push(" " + amigos);
+        mostrarLista();
+        limpiarCaja();
+    }else{
+        alert("Ingresa un nombre válido")
+    }
+
+}
+
+function mostrarLista(){
+    document.getElementById('listaAmigos2').innerHTML = listaAmigos;
+}
+
+function limpiarCaja(){
+    let cajaVacia = document.getElementById('amigo').value = '';
+}
